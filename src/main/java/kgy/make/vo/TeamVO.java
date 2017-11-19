e42843502389ec3756b62d598a4cb41c8e0caea2
lee-PC\lee <woojin126@naver.com>
2017-11-19 17:02:31 +0900

    TeamVo 명

---
+++ src/main/java/kgy/make/vo/TeamVO.java
@@ -0,0 +1,98 @@
+package kgy.make.vo;
+
+public class TeamVO {
+
+	private String team_no;
+	private String team_nm;
+	private String team_gender;
+	private String area;
+	private String alcohol;
+	private String al_num;
+	private String team_comment;
+	private String team_you_commenT;
+	private String team_img_cd;
+	private String team_reg_time;
+	private String team_phone;
+	private String img_file;
+	public String getTeam_no() {
+		return team_no;
+	}
+	public void setTeam_no(String team_no) {
+		this.team_no = team_no;
+	}
+	public String getTeam_nm() {
+		return team_nm;
+	}
+	public void setTeam_nm(String team_nm) {
+		this.team_nm = team_nm;
+	}
+	public String getTeam_gender() {
+		return team_gender;
+	}
+	public void setTeam_gender(String team_gender) {
+		this.team_gender = team_gender;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getAlcohol() {
+		return alcohol;
+	}
+	public void setAlcohol(String alcohol) {
+		this.alcohol = alcohol;
+	}
+	public String getAl_num() {
+		return al_num;
+	}
+	public void setAl_num(String al_num) {
+		this.al_num = al_num;
+	}
+	public String getTeam_comment() {
+		return team_comment;
+	}
+	public void setTeam_comment(String team_comment) {
+		this.team_comment = team_comment;
+	}
+	public String getTeam_you_commenT() {
+		return team_you_commenT;
+	}
+	public void setTeam_you_commenT(String team_you_commenT) {
+		this.team_you_commenT = team_you_commenT;
+	}
+	public String getTeam_img_cd() {
+		return team_img_cd;
+	}
+	public void setTeam_img_cd(String team_img_cd) {
+		this.team_img_cd = team_img_cd;
+	}
+	public String getTeam_reg_time() {
+		return team_reg_time;
+	}
+	public void setTeam_reg_time(String team_reg_time) {
+		this.team_reg_time = team_reg_time;
+	}
+	public String getTeam_phone() {
+		return team_phone;
+	}
+	public void setTeam_phone(String team_phone) {
+		this.team_phone = team_phone;
+	}
+	public String getImg_file() {
+		return img_file;
+	}
+	public void setImg_file(String img_file) {
+		this.img_file = img_file;
+	}
+	
+	
+
+
+	
+	
+	
+	
+	
+}
